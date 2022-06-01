77de8b30799c9b0a9b60901518628b56ba92dfff
wondonghwi <ehdehd1289@naver.com>
2022-06-01 15:01:38 +0900

    💄 mobile tumbnail button custom style,

--- js/carousels.js
+++ js/carousels.js
@@ -4,7 +4,7 @@ const productCarousel = tns({
   navContainer: '.product-carousel .thumbnail-list',
   navAsThumbnails: true,
   arrowKeys: true,
-  // autoplay: true,
+  autoplay: true,
   autoplayHoverPause: true,
   autoplayButtonOutput: false,
   mouseDrag: true,
