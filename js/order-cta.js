b79929129e57f2662a9d7ba7f0e4de0d25fac9e2
wondonghwi <ehdehd1289@naver.com>
2022-06-12 23:34:26 +0900

    ✨ implement toggle bookmarkBtn

--- js/order-cta.js
+++ js/order-cta.js
@@ -1,15 +1,36 @@
 const orderCta = document.querySelector('.order-cta');
 const [orderCtaBookmarkBtn, orderCtaBuyBtn] = orderCta.children;
-
 const orderFormModal = document.querySelector('.order-form-modal');
 const orderModalOverlay = document.querySelector('.overlay');
 
-orderCtaBuyBtn.addEventListener('click', () => {
+const openOrderModal = () => {
   orderFormModal.classList.add('is-open');
   orderModalOverlay.classList.add('is-active');
-});
-
-orderModalOverlay.addEventListener('click', () => {
+};
+const closeOrderModal = () => {
   orderFormModal.classList.remove('is-open');
   orderModalOverlay.classList.remove('is-active');
-});
+};
+const toggleOrderCtaBookmark = () => {
+  const [icon, countSpan] = orderCtaBookmarkBtn.children;
+  const count = parseInt(countSpan.innerHTML.replaceAll(',', ''));
+  let newCount = count;
+  if (orderCtaBuyBtn.classList.contains('is-active')) {
+    icon.classList.add('ic-bookmark');
+    icon.classList.remove('ic-bookmark-filled');
+    newCount -= 1;
+  } else {
+    icon.classList.add('ic-bookmark-filled');
+    icon.classList.remove('ic-bookmark');
+    newCount += 1;
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString('ko-Kr');
+  // countSpan.attributes[0] = `북마크 ${newCount}`;
+
+  orderCtaBuyBtn.classList.toggle('is-active');
+};
+
+orderCtaBuyBtn.addEventListener('click', openOrderModal);
+orderModalOverlay.addEventListener('click', closeOrderModal);
+orderCtaBookmarkBtn.addEventListener('click', toggleOrderCtaBookmark);
