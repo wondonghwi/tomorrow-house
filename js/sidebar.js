7780a4bc811db2d5a448b19b6dd783dd2ab43795
wondonghwi <ehdehd1289@naver.com>
2022-06-12 20:01:38 +0900

    ✨ implement toggle sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,17 @@
+const sidebarMenuBtn = document.querySelector('.gnb-icon-button.is-menu');
+const sidebar = document.querySelector('.sidebar');
+const sidebarOverlay = document.querySelector('.overlay');
+
+const openSidebar = () => {
+  sidebar.classList.add('is-active');
+  sidebarOverlay.classList.add('is-active');
+};
+
+const removeSidebar = () => {
+  sidebar.classList.remove('is-active');
+  sidebarOverlay.classList.remove('is-active');
+};
+
+sidebarMenuBtn.addEventListener('click', openSidebar);
+
+sidebarOverlay.addEventListener('click', removeSidebar);
