5dfca2b0cec30c04fb786af3950c3c5f92f35684
wondonghwi <ehdehd1289@naver.com>
2022-06-14 00:24:40 +0900

    ✨ Implement search-history each deleteBtn

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -3,11 +3,16 @@ const gnbSearchInput = document.querySelector('input');
 const gnbSearchHistory = document.querySelector('.search-history');
 const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol');
 const deleteAllBtn = gnbSearchHistory.querySelector('.search-history-header button');
+const deleteBtnList = gnbSearchHistoryList.querySelectorAll('.delete-button');
 
-const closeGnbSearchHistory = e => {
+const closeGnbSearch = () => {
+  gnbSearchHistory.classList.remove('is-active');
+  window.removeEventListener('click', closeGnbSearchHistoryOutSide);
+};
+
+const closeGnbSearchHistoryOutSide = e => {
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active');
-    window.removeEventListener('click', closeGnbSearchHistory);
+    closeGnbSearch();
   }
 };
 
@@ -15,15 +20,29 @@ const openGnbSearchHistory = () => {
   if (gnbSearchHistoryList.children.length === 0) return;
 
   if (!gnbSearchHistory.classList.contains('is-active')) {
-    window.addEventListener('click', closeGnbSearchHistory);
+    window.addEventListener('click', closeGnbSearchHistoryOutSide);
   }
   gnbSearchHistory.classList.add('is-active');
 };
 
 const deleteAllSearchHistory = () => {
   gnbSearchHistoryList.innerHTML = '';
-  gnbSearchHistory.classList.remove('is-active');
+  closeGnbSearch();
+};
+
+const deleteSearchHistoryItem = (e, index) => {
+  e.stopPropagation();
+  const itemDelete = deleteBtnList[index].parentNode;
+  gnbSearchHistoryList.removeChild(itemDelete);
+
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearch();
+  }
 };
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
 deleteAllBtn.addEventListener('click', deleteAllSearchHistory);
+
+deleteBtnList.forEach((button, index) => {
+  button.addEventListener('click', e => deleteSearchHistoryItem(e, index));
+});
