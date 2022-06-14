a841c0e4ec8ddd5d0c9e36efd850005f5c29d574
wondonghwi <ehdehd1289@naver.com>
2022-06-15 00:01:57 +0900

    🚧 Implement top-panel scroll use getBoundingClientRect()

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,7 +1,9 @@
 const productTab = document.querySelector('.product-tab');
 const productTabBtnList = productTab.querySelectorAll('button');
-
 let currentActiveTab = productTab.querySelector('.is-active');
+
+const TOP_PANEL_MOBILE = 130;
+const TOP_PANEL_DESKTOP = 184;
 
 const toggleActiveTab = index => {
   const tabItem = productTabBtnList[index].parentNode;
@@ -13,6 +15,18 @@ const toggleActiveTab = index => {
   }
 };
 
+const scrollToTabPanel = index => {
+  const tabPanelId = productTabBtnList[index].parentNode.getAttribute('aria-labelledby');
+  const tabPanel = document.querySelector(`#${tabPanelId}`);
+  const scrollTop = tabPanel.getBoundingClientRect().top;
+
+  window.scrollBy({
+    top: window.innerWidth >= 768 ? scrollTop - TOP_PANEL_DESKTOP : scrollTop - TOP_PANEL_MOBILE,
+    behavior: 'smooth',
+  });
+};
+
 productTabBtnList.forEach((button, index) => {
   button.addEventListener('click', () => toggleActiveTab(index));
+  button.addEventListener('click', () => scrollToTabPanel(index));
 });
