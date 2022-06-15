dacaeb600cded2ed1dee749016f6ddf8845f8891
wondonghwi <ehdehd1289@naver.com>
2022-06-15 23:43:14 +0900

    Updates

--- js/search-modal.js
+++ js/search-modal.js
@@ -1,6 +1,6 @@
 const searchModal = document.querySelector('.search-modal');
 const searchOverlay = document.querySelector('.overlay');
-const searchBtn = document.querySelector('.gnb-Icon-button.is-search');
+const searchBtn = document.querySelector('.gnb-icon-button.is-search');
 const searchModalCloseBtn = searchModal.querySelector('.search-modal-form .btn-ghost.btn-40');
 
 searchBtn.addEventListener('click', () => {
